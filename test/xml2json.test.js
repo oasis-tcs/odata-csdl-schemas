0e52966e409d5cc843e57a8353aae0f7890bc7bf
Ralf Handl <ralf.handl@sap.com>
2019-05-13 14:57:16 +0200

    Added xml2json

---
+++ test/xml2json.test.js
@@ -0,0 +1,38 @@
+//TODO: UrlRef with nested annotation
+
+const assert = require('assert');
+const fs = require('fs');
+
+const xml2json = require('../lib/xml2json');
+
+const example1 = fs.readFileSync('examples/csdl-16.1.xml');
+const result1 = require('../examples/csdl-16.1.json');
+
+const example2 = fs.readFileSync('examples/csdl-16.2.xml');
+const result2 = require('../examples/csdl-16.2.json');
+
+const example3 = fs.readFileSync('examples/miscellaneous.xml');
+const result3 = require('../examples/miscellaneous.json');
+
+const example4 = fs.readFileSync('examples/miscellaneous2.xml');
+const result4 = require('../examples/miscellaneous2.json');
+
+describe('Examples', function () {
+
+    it('csdl-16.1', function () {
+        assert.deepStrictEqual(xml2json(example1), result1);
+    })
+
+    it('csdl-16.2', function () {
+        assert.deepStrictEqual(xml2json(example2), result2);
+    })
+
+    it('miscellaneous', function () {
+        assert.deepStrictEqual(xml2json(example3), result3);
+    })
+
+    it('miscellaneous2', function () {
+        assert.deepStrictEqual(xml2json(example4), result4);
+    })
+
+})
