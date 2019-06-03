acaa9ed3f15907ddb2dd6db8a319810bac4968c2
Ralf Handl <ralf.handl@sap.com>
2019-06-03 11:03:53 +0200

    Cleaner exports Updated package.json

--- test/xml2json.test.js
+++ test/xml2json.test.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 const fs = require('fs');
 
-const xml2json = require('../lib/xml2json');
+const csdl = require('../lib/xml2json');
 
 const example1 = fs.readFileSync('examples/csdl-16.1.xml');
 const result1 = require('../examples/csdl-16.1.json');
@@ -20,19 +20,19 @@ const result4 = require('../examples/miscellaneous2.json');
 describe('Examples', function () {
 
     it('csdl-16.1', function () {
-        assert.deepStrictEqual(xml2json(example1), result1);
+        assert.deepStrictEqual(csdl.xml2json(example1), result1);
     })
 
     it('csdl-16.2', function () {
-        assert.deepStrictEqual(xml2json(example2), result2);
+        assert.deepStrictEqual(csdl.xml2json(example2), result2);
     })
 
     it('miscellaneous', function () {
-        assert.deepStrictEqual(xml2json(example3), result3);
+        assert.deepStrictEqual(csdl.xml2json(example3), result3);
     })
 
     it('miscellaneous2', function () {
-        assert.deepStrictEqual(xml2json(example4), result4);
+        assert.deepStrictEqual(csdl.xml2json(example4), result4);
     })
 
 })
