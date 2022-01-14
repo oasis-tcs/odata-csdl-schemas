3d738f3dd0ab422c11f53ad140a7917170a64b6e
D024504 <heiko.theissen@sap.com>
2022-01-14 14:51:34 +0100

    Detect unexpected text content

--- test/xml2json.test.js
+++ test/xml2json.test.js
@@ -405,6 +405,26 @@ describe("Error cases", function () {
     }
   });
 
+  it("unexpected text content", function () {
+    const xml = `<Edmx Version="4.0" xmlns="http://docs.oasis-open.org/odata/ns/edmx">X
+      <DataServices/>
+    </Edmx>`;
+    try {
+      csdl.xml2json(xml);
+      assert.fail("should not get here");
+    } catch (e) {
+      assert.strictEqual(
+        e.message.split("\n")[0],
+        "Unexpected text content in Edmx"
+      );
+      assert.deepStrictEqual(e.parser, {
+        construct: "<DataServices/",
+        line: 2,
+        column: 20,
+      });
+    }
+  });
+
   it("unexpected element in annotation", function () {
     const xml = `<Edmx Version="4.0" xmlns="http://docs.oasis-open.org/odata/ns/edmx">
       <DataServices>
