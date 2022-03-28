0ce628c6c18f7b139b22167465ee472b10ca6419
D024504 <heiko.theissen@sap.com>
2022-03-28 12:00:58 +0200

    loop over examples folder

--- java/Validator.java
+++ java/Validator.java
@@ -2,16 +2,23 @@ import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.XMLConstants;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import org.xml.sax.SAXException;
+
+class XMLFiles implements FilenameFilter {
+	public boolean accept(File dir, String name) {
+		return name.endsWith(".xml");
+	}
+}
 
 public class Validator {
 	public static void main(String[] args) throws IOException, SAXException {
 		var schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		var validator = schemaFactory.newSchema(new StreamSource(new File("schemas/edmx.xsd"))).newValidator();
-		validator.validate(new StreamSource(new File("examples/csdl-16.1.xml")));
-		validator.validate(new StreamSource(new File("examples/csdl-16.2.xml")));
-		validator.validate(new StreamSource(new File("examples/miscellaneous.xml")));
-		validator.validate(new StreamSource(new File("examples/miscellaneous2.xml")));
+		for (var file : new File("examples").listFiles(new XMLFiles())) {
+			System.out.println(file.getName());
+			validator.validate(new StreamSource(file));
+		}
 	}
 }
