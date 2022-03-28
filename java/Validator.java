f38cd5c2dd6abcc2662ccfb68c9ce199b135160f
D024504 <heiko.theissen@sap.com>
2022-03-28 11:49:30 +0200

    Test all CSDL files

--- java/Validator.java
+++ java/Validator.java
@@ -9,6 +9,9 @@ public class Validator {
 	public static void main(String[] args) throws IOException, SAXException {
 		var schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 		var validator = schemaFactory.newSchema(new StreamSource(new File("schemas/edmx.xsd"))).newValidator();
+		validator.validate(new StreamSource(new File("examples/csdl-16.1.xml")));
+		validator.validate(new StreamSource(new File("examples/csdl-16.2.xml")));
 		validator.validate(new StreamSource(new File("examples/miscellaneous.xml")));
+		validator.validate(new StreamSource(new File("examples/miscellaneous2.xml")));
 	}
 }
