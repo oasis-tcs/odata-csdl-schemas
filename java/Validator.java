741848b2f9c519ffef1e41d703132337ae229de0
D024504 <heiko.theissen@sap.com>
2022-03-28 11:43:05 +0200

    Simplified test

--- java/Validator.java
+++ java/Validator.java
@@ -8,10 +8,7 @@ import org.xml.sax.SAXException;
 public class Validator {
 	public static void main(String[] args) throws IOException, SAXException {
 		var schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		StreamSource[] schemaFiles = new StreamSource[2];
-		schemaFiles[0] = new StreamSource(new File("schemas/edmx.xsd"));
-		schemaFiles[1] = new StreamSource(new File("schemas/edm.xsd"));
-		var validator = schemaFactory.newSchema(schemaFiles).newValidator();
+		var validator = schemaFactory.newSchema(new StreamSource(new File("schemas/edmx.xsd"))).newValidator();
 		validator.validate(new StreamSource(new File("examples/miscellaneous.xml")));
 	}
 }
